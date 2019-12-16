f386705bf8595c33477974cba1edb90f1c6d7382
RaquelMarques97 <araquelfmarques@gmail.com>
2019-12-16 10:22:18 +0000

    express router

---
+++ my-app/back/routes/auth/auth.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.post('/signup', function(req, res, next) {
+
+    res.send('I am in POST signup');
+    
+});
+    
+module.exports = router;
